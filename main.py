68eefb9ef2b5e3f6c5da1443d4acf426fefa2555
mahabul546 <hosainmahabul121@gmail.com>
2026-09-12 13:16:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 from datetime import datetime
 import json
 
-BOT_TOKEN = "-এখানে-তোমার-আসল-টোকেন-বসাও"
+BOT_TOKEN = "8822168901:AAFR12UNn-dqcNLd8q2HzGaWx5wybxyUGIs-এখানে-তোমার-আসল-টোকেন-বসাও"
 ADMIN_ID = 7834320405
 CHANNEL_USERNAME = "@mhabul546"
 NOTICE_FILE = "notice.json"
